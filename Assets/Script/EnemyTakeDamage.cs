82dd325ef32836cc06627bd7b9d220be4dc491ce
Yaroslav4561 <jaroslawmatuh2@gmail.com>
2025-03-17 13:55:35 +0200

    bild 0.0.3
    - Зброя наносить шкоду
    - Баг анімації смерті ворога

--- Assets/Script/EnemyTakeDamage.cs
+++ Assets/Script/EnemyTakeDamage.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DamageScript : MonoBehaviour
 {
-    public int damageAmount = 2;
+    public int damageAmount = 1;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Enemy")
         {
             other.GetComponent<EnemyScript>().Damage(damageAmount);
+            Debug.LogError(other.gameObject.name);
         }
     }
 }
